d06296e9f83bfaf5b2e574b31655a4558d757128
VaniaStoicheva <stoychevavania@gmail.com>
2020-04-06 16:34:09 +0300

    core. shared folder

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <app-content>
+        <template v-slot:nav>
+             <ul>
+                        <li>
+                            <a href="#">JS Conditional Statements</a>
+                        </li>
+                        <li>
+                            <a href="#">JS Loops</a>
+                        </li>
+                        <li>
+                            <a href="#">JS Arrays</a>
+                        </li>
+                        <li>
+                            <a href="#">JS Objects</a>
+                        </li>
+                    </ul>
+        </template>
+    </app-content>
+</template>
+
+<script>
+import AppContent from './shared/Content.vue';
+
+export default {
+components:{
+    AppContent
+}    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/core/Navigation.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="navigation">
+                <ul>
+                    <li v-for="item in navItems" :key="item.id"><a>{{item.name}}</a></li>
+                   
+                </ul>
+            </div>
+            
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        navItems:{
+            type:Array,
+            required:true
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+li a {
+    cursor: pointer;
+}
+</style>
